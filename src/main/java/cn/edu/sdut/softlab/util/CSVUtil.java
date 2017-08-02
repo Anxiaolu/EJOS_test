34ab48de2ce7d26301c7ecab1ecb6ecc40c192bb
Anxiaolu <1466022881@qq.com>
2017-08-02 21:43:43 +0800

    完成文件输出,其他待定

--- src/main/java/cn/edu/sdut/softlab/util/CSVUtil.java
+++ src/main/java/cn/edu/sdut/softlab/util/CSVUtil.java
@@ -17,17 +17,21 @@ import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.nio.charset.Charset;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.faces.bean.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.junit.Test;
 
@@ -44,6 +48,9 @@ public class CSVUtil {
 
     @Inject
     StudentFacade studentService;
+    
+    @Inject
+    EntityManager em;
 
     /**
      *
@@ -112,7 +119,7 @@ public class CSVUtil {
      */
     public void writeWithStudentByTeam(String path,char delimiter,Integer stu_team_id) throws Exception{
         try {
-            File f = new File("/home/huanlu/" + path + studentService.findByStuId(stu_team_id).getName() + ".csv");
+            File f = new File("/home/huanlu/" + path + "/" + studentService.findByStuId(stu_team_id).getTeam().getName() + ".csv");
             OutputStream output = new FileOutputStream(f);
             CsvWriter csvWriter = new CsvWriter(output, delimiter, Charset.forName("UTF-8"));
             List<Student> stus = studentService.findByTeam(stu_team_id);
@@ -126,66 +133,76 @@ public class CSVUtil {
         }
     }
 
-    public ResultSetMetaData getResultSetMetaData() throws Exception {
-        String sql = "select * from teacher";
-        PreparedStatement ps = null;
-        try {
-            ps = studentService.getEm().unwrap(Connection.class).prepareStatement(sql);
-        } catch (SQLException ex) {
-            Logger.getLogger(CSVUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        ResultSet resultSet = null;
-        try {
-            resultSet = ps.executeQuery();
-        } catch (SQLException ex) {
-            Logger.getLogger(CSVUtil.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public ResultSetMetaData getResultSetMetaData(String table_name) throws Exception {
+        String sql = "select * from " + table_name;
+        PreparedStatement ps = studentService.getEm().unwrap(Connection.class).prepareStatement(sql);
+        ResultSet resultSet = ps.executeQuery();
         return resultSet.getMetaData();
     }
     
-    
-    public void outOfCsv(String filePath, String sql) throws Exception {
-        // 判断文件是否存在,存在则删除,然后创建新表格
-        File tmp = new File(filePath);
-        if (tmp.exists()) {
-            if (tmp.delete()) {
-                logger.info(filePath);
-            }
-        }
-        
-//        Query query = studentService.getHibernateQuery(sql);
-//        
-//        query.setResultTransformer(
-//                //ToListResultTransformer.INSTANCE
-//                new ResultTransformer() {
-//            @Override
-//            public Object transformTuple(Object[] tuple, String[] aliases) {
-//                Integer id = (Integer) tuple[0];
-//                return ;
-//            }
-//
-//            @Override
-//            public List transformList(List collection) {
-//                return Collections.unmodifiableList(collection);
-//            }
-//        }
-//        );
-
-        // 创建CSV写对象
-        CsvWriter csvWriter = new CsvWriter(filePath, ',', Charset.forName("UTF-8"));
-
-        // 数据查询开始
-        PreparedStatement preparedStatement = studentService.getEm().unwrap(Connection.class).prepareStatement(sql);
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-        // 获取结果集表头
+    /**
+     * 获取数据表的表头信息
+     * @param table_name
+     * @return String[] 返回表头信息
+     * @throws Exception 
+     */
+    public Map<String,Object> getColumnName(String table_name) throws Exception {
+        String sql = "select * from " + table_name;
+        PreparedStatement ps = em.unwrap(java.sql.Connection.class).prepareStatement(sql);
+        ResultSet resultSet = ps.executeQuery();
         ResultSetMetaData md = resultSet.getMetaData();
         int columnCount = md.getColumnCount();
-        logger.info("返回结果字段个数:" + columnCount);
-
-        // 文件输出
-        csvWriter.flush();
-        csvWriter.close();
+        JSONArray columnName = new JSONArray();
+        for(int i = 1; i <= columnCount; i++)
+        {
+            JSONObject object = new JSONObject();
+            logger.info(md.getColumnName(i));
+            columnName.add(object);
+        }
+        return null;
+    }
+    
+    public void test(String table_name) throws SQLException{
+        EntityManagerFactory emf = em.getEntityManagerFactory();
+        Map<String,Object> emfproperties = emf.getProperties();
+        //logger.info(emfproperties.toString());
+        List<String> key = new ArrayList<>(emfproperties.keySet());
+//        for (String s : key) {
+//            System.out.println("000 " + s);
+//        }
+        org.hibernate.engine.spi.SessionImplementor sessionImp = (org.hibernate.engine.spi.SessionImplementor) em.getDelegate();
+        DatabaseMetaData metadata = null;
+        try {
+            metadata = sessionImp.connection().getMetaData();
+            logger.info(metadata.toString());
+            ResultSet res = metadata.getIndexInfo("ejos", null, table_name, false, true);
+            System.out.println("List of columns: ");
+            while (res.next()) {                
+                System.out.println("1 " + res.getString(1));
+                System.out.println("2 " + res.getString(2));
+                System.out.println("3 " + res.getString(3));
+                System.out.println("4 " + res.getString(4));
+                System.out.println("5 " + res.getString(5));
+                System.out.println("6 " + res.getString(6));
+                System.out.println("7 " + res.getString(7));
+                System.out.println("8 " + res.getString(8));
+                System.out.println("9 " + res.getString(9));
+                System.out.println("10 " + res.getString(10));
+            }
+//            while (res.next()) {
+//                System.out.println(
+//                        "  " + res.getString("TABLE_SCHEM")
+//                        + ", " + res.getString("TABLE_NAME")
+//                        + ", " + res.getString("COLUMN_NAME")
+//                        + ", " + res.getString("TYPE_NAME")
+//                        + ", " + res.getInt("COLUMN_SIZE")
+//                        + ", " + res.getInt("NULLABLE"));
+//            }
+            res.close();
+            System.out.println();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
     
 }
